83b37978e761c3f6f568a53ad6f011d8d00a61e8
Oleh Tyshchenko <sokol.wise@gmail.com>
2023-12-29 16:57:16 +0200

    created project

---
+++ app/src/main/java/com/example/financemonitoring/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.financemonitoring.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.financemonitoring.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
