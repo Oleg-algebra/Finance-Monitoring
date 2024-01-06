aeb4d609cff0b96d448df5bdd462e9103bd5c7cf
Oleh Tyshchenko <sokol.wise@gmail.com>
2024-01-06 21:00:20 +0200

    ReportActivity: finished

--- app/src/main/java/com/example/financemonitoring/presentation/report/ReportActivity.kt
+++ app/src/main/java/com/example/financemonitoring/presentation/report/ReportActivity.kt
@@ -7,6 +7,11 @@ import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.financemonitoring.R
+import com.example.financemonitoring.domain.Filter
+import com.example.financemonitoring.domain.FinanceRecord
+import com.example.financemonitoring.domain.IntentPreprocessing
+import com.example.financemonitoring.domain.filter_comands.FilterChain
+import java.time.LocalDate
 
 class ReportActivity : AppCompatActivity() {
 
@@ -20,11 +25,35 @@ class ReportActivity : AppCompatActivity() {
     private lateinit var viewModel: ReportViewModel
     private lateinit var reportAdapter: ReportAdapter
 
+    private lateinit var filter: Filter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
         initViews()
+        parseDateFrame()
         registerLiveData()
+    }
+
+
+
+    fun parseDateFrame(){
+        filter = IntentPreprocessing.parseIntent(intent)
+        val fromDate = filter.fromDate?.toString()
+            ?.split("-".toRegex())
+            ?.reversed()
+            ?.joinToString(separator = ".")
+        val toDate = filter.toDate?.toString()
+            ?.split("-".toRegex())
+            ?.reversed()
+            ?.joinToString(separator = ".")
+
+        dateFrameTV.text = fromDate?.let { "From $it to" }
+            ?.let { "$it ${toDate ?: "now"}" }
+            ?: toDate?.let { "From beginning to $it" }
+            ?: "For all time"
+
+
 
     }
 
@@ -44,8 +73,45 @@ class ReportActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[ReportViewModel::class.java]
         viewModel.liveData.observe(this){
-            reportAdapter.submitList(it)
+            reportAdapter.submitList(createSummary(FilterChain.execute(filter,it)))
         }
 
+    }
+
+    fun createSummary(records: List<FinanceRecord>): List<FinanceRecord>{
+        val newList = mutableListOf<FinanceRecord>()
+        records.forEach { r ->
+            newList.find { it.category == r.category }
+                ?.run {
+                    newList.remove(this)
+                    newList.add(this.copy(change = this.change + r.change))
+                }?: newList.add(r)
+        }
+        var incomeValue = 0L
+        var investmentsValue = 0L
+        newList.forEach {
+            if(it.change > 0){
+                incomeValue += it.change
+            }else{
+                investmentsValue += it.change
+            }
+        }
+
+        incomeTV.text = incomeValue.toString()
+        investmentTV.text = investmentsValue.toString()
+        val outcome = incomeValue + investmentsValue
+        outcomeTV.text = outcome.toString()
+        outcomeIcon.setImageResource(
+            if(outcome<0){
+                R.drawable.red_triangle
+            }else{
+                R.drawable.green_triangle
+            }
+        )
+        newList.replaceAll {
+            it.copy(date = LocalDate.now())
+        }
+
+        return newList
     }
 }
